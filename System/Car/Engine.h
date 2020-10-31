c7a7289f48540ccdc26e0f4c15265d9f9742fb5b
rbieldt <ruanbeeld9j@gmail.com>
2020-10-31 21:04:23 +0200

    Engine structure added

--- System/Car/Engine.h
+++ System/Car/Engine.h
@@ -5,4 +5,16 @@
 #ifndef COS214PROJECT_ENGINE_H
 #define COS214PROJECT_ENGINE_H
 
+#include "Component.h"
+
+class Engine: public Component{
+private:
+public:
+    Engine();
+    Engine(Engine* c);
+    ~Engine();
+    bool test();
+    Component* clone();
+
+};
 #endif //COS214PROJECT_ENGINE_H
