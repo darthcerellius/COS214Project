0e5376e2c987eb9d57e7f059e62fae7de51be14b
Ivan <u18028200@tuks.co.za>
2020-11-01 11:52:22 +0200

    Created Tyre Sub folder in Car directory and added empty Tyre classes

---
+++ System/Car/Tyre/Tyre.h
@@ -0,0 +1,14 @@
+//
+// Created by Ivan on 2020/11/01.
+//
+
+#ifndef COS214PROJECT_TYRE_H
+#define COS214PROJECT_TYRE_H
+#include "../Component.h"
+
+class Tyre {
+
+};
+
+
+#endif //COS214PROJECT_TYRE_H
