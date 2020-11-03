6ac95855ffe866050fc83e8fde345fd9b6898d0c
J9Burger <u10085582@tuks.co.za>
2020-11-03 17:32:21 +0200

    u10085582-Janine Burger: Raceweekend edited, added date calculations

--- System/Calendar/RaceWeekend.cpp
+++ System/Calendar/RaceWeekend.cpp
@@ -7,13 +7,19 @@
 RaceWeekend::RaceWeekend(int day, int month, int year, std::string location, std::string trackName){
     this->location = location;
     this->trackName = trackName;
+    date = new Date();
+    date->d = day;
+    date->m = month;
+    date->y = year;
 }
 
 void RaceWeekend::setDate(int day, int month, int year){
-
+    date->d = day;
+    date->m = month;
+    date->y = year;
 }
 
-int RaceWeekend::getDate(){
+Date* RaceWeekend::getDate(){
     return date;
 }
 
@@ -30,4 +36,32 @@ void RaceWeekend::setName(std::string trackName){
 }
 std::string RaceWeekend::getName(){
     return trackName;
+}
+
+int RaceWeekend::daysBetween(Date* date1, Date* date2){
+    int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    long int num1 = date1->y * 365 + date1->d;
+    for(int i = 0; i < date1->m - 1; i++){
+        num1 += monthDays[i];
+    }
+    num1 += countLeapYears(date1);
+
+    long int num2 = date2->y * 365 + date2->d;
+    for(int i = 0; i < date2->m - 1; i++) {
+        num2 += monthDays[i];
+    }
+    num2 += countLeapYears(date2);
+
+    return (num2 - num1);
+}
+
+int RaceWeekend::countLeapYears(Date* date){
+    int years = date->y;
+
+    if (date->m <= 2) {
+        years--;
+    }
+
+    return years/4 - years/100 + years/400;
 }
--- System/Calendar/RaceWeekend.h
+++ System/Calendar/RaceWeekend.h
@@ -6,17 +6,23 @@
 #ifndef COS214PROJECT_RACEWEEKEND_H
 #define COS214PROJECT_RACEWEEKEND_H
 
+struct Date {
+    int d, m, y;
+};
+
 class RaceWeekend{
 public:
     RaceWeekend(int day, int month, int year, std::string location, std::string trackName);
     void setDate(int day, int month, int year);
-    int getDate();
+    Date* getDate();
     void setLocation(std::string location);
     std::string getLocation();
     void setName(std::string trackName);
     std::string getName();
+    int daysBetween(Date* date1, Date* date2);
+    int countLeapYears(Date* date);
 private:
-    int date;
+    Date* date;
     std::string location;
     std::string trackName;
 };
