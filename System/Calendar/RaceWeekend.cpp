44e25113ac40ff6e80c53d7014a3714ba7d76744
J9Burger <u10085582@tuks.co.za>
2020-11-03 15:48:48 +0200

    u10085582-Janine Burger: Raceweekend edited, added RaceTrack.txt

--- System/Calendar/RaceWeekend.cpp
+++ System/Calendar/RaceWeekend.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "RaceWeekend.h"
+
+RaceWeekend::RaceWeekend(int day, int month, int year, std::string location, std::string trackName){
+    this->location = location;
+    this->trackName = trackName;
+}
+
+void RaceWeekend::setDate(int day, int month, int year){
+
+}
+
+int RaceWeekend::getDate(){
+    return date;
+}
+
+void RaceWeekend::setLocation(std::string location){
+    this->location = location;
+}
+
+std::string RaceWeekend::getLocation(){
+    return location;
+}
+
+void RaceWeekend::setName(std::string trackName){
+    this->trackName = trackName;
+}
+std::string RaceWeekend::getName(){
+    return trackName;
+}
--- System/Calendar/RaceWeekend.h
+++ System/Calendar/RaceWeekend.h
@@ -2,12 +2,23 @@
 // Created by janine on 2020/11/02.
 //
 
+#include <string>
 #ifndef COS214PROJECT_RACEWEEKEND_H
 #define COS214PROJECT_RACEWEEKEND_H
 
 class RaceWeekend{
-
-
+public:
+    RaceWeekend(int day, int month, int year, std::string location, std::string trackName);
+    void setDate(int day, int month, int year);
+    int getDate();
+    void setLocation(std::string location);
+    std::string getLocation();
+    void setName(std::string trackName);
+    std::string getName();
+private:
+    int date;
+    std::string location;
+    std::string trackName;
 };
 
 #endif //COS214PROJECT_RACEWEEKEND_H
