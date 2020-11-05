8efc8b8a8c4d32cc465181ffdf7e9c3f7a0d08e4
HannesKirsten <u19049014@tuks.co.za>
2020-11-05 14:52:30 +0200

    u19049014: Removed AbstractEngineeringTeam and added everything to EngineeringTeam

--- System/Engineering/EngineeringTeam.cpp
+++ System/Engineering/EngineeringTeam.cpp
@@ -26,3 +26,19 @@ EngineeringTeam::EngineeringTeam() {
     engineFactory=new EngineFactory();
     chassisFactory=new ChassisFactory();
 }
+
+void EngineeringTeam::buildCar() {
+    car=new Car();
+    car->add(buildAerodynamicsComponent());
+    car->add(buildChassis());
+    car->add(buildEngine());
+    car->add(buildElectronicsComponent());
+}
+
+Car *EngineeringTeam::getCar() {
+    return car;
+}
+
+void EngineeringTeam::serviceCar() {
+    cout<<"Car has been serviced"<<endl;
+}
--- System/Engineering/EngineeringTeam.h
+++ System/Engineering/EngineeringTeam.h
@@ -10,24 +10,25 @@
 #include "ConcreteFactories/EngineFactory.h"
 #include "ConcreteFactories/ElectronicsFactory.h"
 #include "ConcreteFactories/ChassisFactory.h"
+#include "../Car/Car.h"
 
 class EngineeringTeam : public AbstractEngineeringTeam{
 private:
-    //double AerodynamicsBudget;//I think this should be removed and the AbstractEngineeringTeam should just become the EngineeringTeam
-    //double EngineBudget;//
-    //double ElectronicsBudget;//
-    //double ChassisBudget;//
     AerodynamicsFactory* aerodynamicsFactory;
     EngineFactory* engineFactory;
     ElectronicsFactory* electronicsFactory;
     ChassisFactory* chassisFactory;
-protected:
-    AerodynamicsComponent* buildAerodynamicsComponent() override;
-    Engine* buildEngine() override;
-    ElectronicsComponent* buildElectronicsComponent() override;
-    Chassis* buildChassis() override;
+
+    Car* car;
+    Engine* buildEngine();
+    AerodynamicsComponent* buildAerodynamicsComponent();
+    ElectronicsComponent* buildElectronicsComponent();
+    Chassis* buildChassis();
 public:
     EngineeringTeam();
+    void buildCar();
+    Car* getCar();
+    void serviceCar();
 };
 
 
