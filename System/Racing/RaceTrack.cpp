12c14d5fc71edc62aa3ff44e8616561640327eef
J9Burger <u10085582@tuks.co.za>
2020-11-05 17:01:32 +0200

    u10085582-Janine Burger: editing RaceTrack

--- System/Racing/RaceTrack.cpp
+++ System/Racing/RaceTrack.cpp
@@ -4,7 +4,31 @@
 
 #include "RaceTrack.h"
  //subject in observer
- //RaceTrack(gets car, pit crew);
  //300/length , round to nearest
  //for(0<laps) {random num for events (randbetween 5 - 80) 5 = pop tyre
  // pop tyre 5%, oil leak 10%, .....
+
+RaceTrack::RaceTrack(Car* _car, PitCrew* _crew, RaceInfo* _info){
+    car = _car;
+    crew = _crew;
+    info = _info;
+}
+
+void RaceTrack::numLaps(){
+    double km = info->length;
+    laps = round(300/km);
+}
+
+int RaceTrack::getLaps(){
+    return laps;
+}
+
+void RaceTrack::race(){
+    for(int i = 0; i < laps, i++){
+
+    }
+}
+
+void notifyPitcrew(){
+
+}
--- System/Racing/RaceTrack.h
+++ System/Racing/RaceTrack.h
@@ -4,12 +4,25 @@
 
 #ifndef COS214PROJECT_RACETRACK_H
 #define COS214PROJECT_RACETRACK_H
+#include <cmath>
+#include "../Car/Car.h"
+#include "../RacingStrategy/PitCrew/PitCrew.h"
+#include "../Calendar/RaceWeekend.h"
 
+class TeamManager;
 
 class RaceTrack {
 public:
-    RaceTrack();
-    private
+    RaceTrack(Car* _car, PitCrew* _crew, RaceInfo* _info);
+    void numLaps();
+    int getLaps();
+    void race();
+    void notifyPitcrew();
+private:
+    Car* car;
+    PitCrew* crew;
+    RaceInfo* info;
+    int laps;
 };
 
 
