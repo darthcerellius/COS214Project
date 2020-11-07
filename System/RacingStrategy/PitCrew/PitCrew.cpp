c4ae374ba62f1510a4848c7aec51ffa99149d1f7
Marco Oosthuizen <Marco.Oosthuizen.2000@gmail.com>
2020-11-07 13:23:01 +0200

    u19012013: Fixed segfault in PitCrew.cpp

--- System/RacingStrategy/PitCrew/PitCrew.cpp
+++ System/RacingStrategy/PitCrew/PitCrew.cpp
@@ -59,7 +59,6 @@ void PitCrew::observeNewEvent(RaceEvent * newEvent) {
     RaceEvent* temp = currentEvent; //NoEvent
     currentEvent = newEvent;
     respondToEvent();
-    delete currentEvent;
     currentEvent = temp;
 }
 
