e1706ec96e8703189f83c55f9714f3a4fd0e2038
Marco Oosthuizen <Marco.Oosthuizen.2000@gmail.com>
2020-11-08 12:32:07 +0200

    u19012013: Added Doxygen commentary to GeneralRaceStrategy.h, Instruction.h, PitCrew.h, RaceEvent.h and StrategyTeam.h

--- System/RacingStrategy/Strategies/GeneralRaceStrategy.h
+++ System/RacingStrategy/Strategies/GeneralRaceStrategy.h
@@ -1,12 +1,17 @@
 #ifndef GENERALRACESTRATEGY_H
 #define GENERALRACESTRATEGY_H
 #include <iostream>
+/**
+ * An Abstract class that defines an interface for the general strategies that will be used by the racecar. Acts
+ * as the Strategy participant in the GeneralStrategy Strategy pattern. The child classes act as the Concrete
+ * Strategy Participants.
+ */
 class GeneralRaceStrategy{
 public:
     GeneralRaceStrategy();
     virtual ~GeneralRaceStrategy();
-    virtual void executeStrategy() = 0;
-    virtual GeneralRaceStrategy* clone() = 0;
+    virtual void executeStrategy() = 0; ///returns a string describing the strategy used by the car. Defined individually in child classes
+    virtual GeneralRaceStrategy* clone() = 0;///Returns a deep copy of the current object. Defined individually in the child classes.
 };
 
 #endif
