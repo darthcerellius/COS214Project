f881bab1a5b8060a0aed8caff0b94cc78ca4465a
rbieldt <ruanbeeld9j@gmail.com>
2020-11-09 18:49:34 +0200

    u13145992:Added doxygen comments

--- System/Car/ElectronicsComponent.h
+++ System/Car/ElectronicsComponent.h
@@ -40,6 +40,10 @@ public:
 
     bool softwareTest() override;
 
+    /**
+     * A method which sets the mode of the electronics to the passed in string
+     * @param mode - The string description of the mode to be set
+     * */
     void setMode(string mode);
 };
 #endif //COS214PROJECT_ELECTRONICSCOMPONENT_H
